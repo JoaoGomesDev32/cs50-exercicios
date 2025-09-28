6cfb2cb0c1c6c4f3659287069cd15810cc147700
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-09-28 11:27:13 +0000

    Sun, Sep 28, 2025, 12:27 PM +01:00

---
+++ aula01/exercicio1-mario/mario.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Tamanho: ");
+    }
+    while (n < 1 && n > 8);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
