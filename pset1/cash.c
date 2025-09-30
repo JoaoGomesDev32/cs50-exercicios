e26ee333a0a287d68d74af9adf392f6dde9d1c69
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-09-30 15:28:47 +0000

    Tue, Sep 30, 2025, 4:28 PM +01:00

--- pset1/cash.c
+++ pset1/cash.c
@@ -16,4 +16,25 @@ int main(void)
     int cents = round(dollars * 100);
 
     int coins = 0;
+
+    // conta quantas moedas de 25 cabem
+    coins += cents / 25;
+    cents %= 25;
+
+    // conta quantas moedas de 10 cabem
+    coins += cents / 10;
+    cents %= 10;
+
+    // conta quantas moedas de 5 cabem
+    coins += cents / 5;
+    cents %= 5;
+
+    // conta quantas moedas de 1 cabem
+    coins += cents / 1;
+    cents %= 1;
+
+    // resultado final
+    printf("%i\n", coins);
+
+    return 0;
 }
