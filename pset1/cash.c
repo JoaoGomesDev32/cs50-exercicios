951cb79f8cd26199fd07c6f423beab64e0dfc0fb
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-09-30 15:28:08 +0000

    Tue, Sep 30, 2025, 4:28 PM +01:00

--- pset1/cash.c
+++ pset1/cash.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
-    
+    float dollars;
+    // pede um valor positivo
+    do
+    {
+        dollars = get_float("Troco devido: ");
+    }
+    while (dollars < 0);
+
+    // converte para centavos(arredonda para evitar erros)
+    int cents = round(dollars * 100);
+
+    int coins = 0;
 }
