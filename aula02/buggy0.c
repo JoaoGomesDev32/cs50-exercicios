7fb71daae232444f1f612cffedbd9ec7a0b4d1b2
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-10-01 20:59:17 +0100

    aula debugg ide falhou

---
+++ aula02/buggy0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Imprime 10 hashes
+    for(int i = 0; i <= 10; i ++)
+    {
+        printf("i vale agora %i\n",i);
+        printf("# \n");
+    }
+}
