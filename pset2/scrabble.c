0140da80d19a9e838f54d149c2b657abb7c7df00
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-10-02 17:46:44 +0000

    Thu, Oct 2, 2025, 6:46 PM +01:00

--- pset2/scrabble.c
+++ pset2/scrabble.c
@@ -5,4 +5,12 @@ int main(void)
 {
     string Jogador1 = get_string("Inserir palavara do jogador 1: ");
     string Jogador2 = get_string("Inserir palavara do jogador 2: ");
+
+    if(Jogador1 > Jogador2)
+    {
+        printf("Jogador 1 Ganhou!");
+    } else
+    {
+        printf("Jogador 2 Ganhou!");
+    }
 }
